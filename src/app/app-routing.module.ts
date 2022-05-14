88416f2d8591de9fb3b8a19612c639feabe6b118
Mura <murasaki1934@gmail.com>
2022-05-14 17:07:38 +0900

    詳細コンポーネントの追加、商品コンポーネントの追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailComponent } from './product/product-detail/product-detail.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
 
 const routes: Routes = [
-  { path: "", component: ProductListComponent }
+  { path: "", component: ProductListComponent },
+  { path: "detail", component: ProductDetailComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
 import { AppRoutingModule } from './app-routing.module';
-import { ProductListComponent } from './product-list/product-list.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import { ProductDetailComponent } from './product/product-detail/product-detail.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductDetailComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
