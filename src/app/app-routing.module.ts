44de353922f1bca7ff9596d132697411d02a9455
Mura <murasaki1934@gmail.com>
2022-05-14 15:37:50 +0900

    ProductListComponentを追加。ルーティングモジュールを修正。

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
 
-
+const routes: Routes = [
+  { path: "", component: ProductListComponent }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
